87abfa2209b2dc86ad3ee4ac4dbacc02d89575a6
Sarthak Khare <46642529+sarthakkhare945@users.noreply.github.com>
2025-05-20 00:19:07 +0530

    initial commit

---
+++ src/components/Playground/LanguageSelector.jsx
@@ -0,0 +1,49 @@
+import {
+    Box,
+    Button,
+    Menu,
+    MenuButton,
+    MenuItem,
+    MenuList,
+    Text,
+  } from "@chakra-ui/react";
+  import { LANGUAGE_VERSIONS } from "../../../constants";
+  
+  const languages = Object.entries(LANGUAGE_VERSIONS);
+  const ACTIVE_COLOR = "blue.400";
+  
+  const LanguageSelector = ({ language, onSelect }) => {
+    return (
+      <Box ml={2} mb={4}>
+        <Text mb={2} fontSize="lg">
+          Language:
+        </Text>
+        <Menu isLazy>
+          <MenuButton as={Button}>{language}</MenuButton>
+          {console.log('language check--->',language)}
+          <MenuList bg="#110c1b">
+            {languages.map(([lang, version]) => (
+              <MenuItem
+                key={lang}
+                color={lang === language ? ACTIVE_COLOR : ""}
+                bg={lang === language ? "black" : "black"}
+                _hover={{
+                  color: ACTIVE_COLOR,
+                  bg: "black",
+                }}
+                onClick={() => onSelect(lang)}
+              >
+                {lang}
+                &nbsp;
+                <Text as="span" color="black" fontSize="sm">
+                  ({version})
+                </Text>
+              </MenuItem>
+            ))}
+          </MenuList>
+        </Menu>
+      </Box>
+    );
+  };
+  export default LanguageSelector;
+  
